343ba47e05916b6831efb40756a6a80a8b84c383
mennitise@gmail.com <mennitise@gmail.com@98f5ffe9-1009-ce00-be44-1651309dea88>
2013-11-06 19:26:47 +0000

    Se agregó a tablero y casillero la capacidad de guardar vehiculos.

--- src/Tablero/Casillero.java
+++ src/Tablero/Casillero.java
@@ -1,13 +1,16 @@
 package Tablero;
 
 import Obstaculos.Obstaculo;
+import Vehiculos.Vehiculo;
 
 public class Casillero {
 	
 	private Obstaculo contenido;
+	private Vehiculo unVehiculo;
 	
 	public Casillero(){
-		this.contenido = null;
+		this.contenido  = null;
+		this.unVehiculo = null;
 	}
 	
 	public boolean estaOcupado() {
@@ -17,9 +20,30 @@ public class Casillero {
 	public boolean estaVacio() {
 		return (this.contenido == null);
 	}
+	
 	public void ponerObstaculoOSorpresa(Obstaculo unObstaculo) {
 		this.contenido = unObstaculo;
 		
 	}
+	
+	public void ponerVehiculo(Vehiculo unVehiculo){
+		this.unVehiculo = unVehiculo;
+	}
+	
+	public boolean hayUnVehiculo(){
+		return (this.unVehiculo != null);
+	}
+	
+	public Vehiculo getVehiculoEnCasillero(){
+		return unVehiculo;
+	}
 
+	public Vehiculo quitarVehiculo(){
+		if (!this.hayUnVehiculo()){
+			return null;
+		}
+		Vehiculo unVehiculo = this.unVehiculo;
+		this.unVehiculo = null;
+		return unVehiculo;
+	}
 }
--- test/TableroTest/CasilleroTest.java
+++ test/TableroTest/CasilleroTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import Obstaculos.ControlPolicial;
-import Obstaculos.Obstaculo;
 import Tablero.Casillero;
+import Vehiculos.Vehiculo;
 
 public class CasilleroTest {
 
@@ -27,5 +27,37 @@ public class CasilleroTest {
 		assertTrue(casillero.estaOcupado());
 	}
 	
-
+	@Test
+	public void testCasilleroGuardaVehiculo() {
+		Casillero casillero = new Casillero();
+		Vehiculo unVehiculo = new Vehiculo();
+		
+		casillero.ponerVehiculo(unVehiculo);
+		assertTrue(casillero.hayUnVehiculo());
+	}
+	
+	@Test
+	public void testCasilleroDevuelveVehiculoGuardadoEnEl() {
+		Casillero casillero = new Casillero();
+		Vehiculo unVehiculo = new Vehiculo();
+		
+		casillero.ponerVehiculo(unVehiculo);
+		assertTrue(casillero.getVehiculoEnCasillero() == unVehiculo);
+	}
+	
+	@Test
+	public void testCasilleroQuitarUnVehiculoDevuelveVehiculo() {
+		Casillero casillero = new Casillero();
+		Vehiculo unVehiculo = new Vehiculo();
+		
+		casillero.ponerVehiculo(unVehiculo);
+		assertTrue(casillero.quitarVehiculo() == unVehiculo);
+	}
+	
+	@Test
+	public void testCasilleroQuitarUnVehiculoDeCasilleroSinVehiculoDevuelveNull() {
+		Casillero casillero = new Casillero();
+		assertTrue(casillero.quitarVehiculo() == null);
+	}
+	
 }
