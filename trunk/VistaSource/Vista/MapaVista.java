8e7bfdd612005a8e519387db358547da2cbea59c
moncholo06@gmail.com <moncholo06@gmail.com@98f5ffe9-1009-ce00-be44-1651309dea88>
2013-11-24 19:12:18 +0000

    Pair Programming entre Juan Laura y Juan Costa. Subimos controlador de movimientos

--- trunk/VistaSource/Vista/MapaVista.java
+++ trunk/VistaSource/Vista/MapaVista.java
@@ -1,46 +1,92 @@
 package Vista;
 import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JButton;
 
+import GestorDeMovimientos.GestorDeMovimientos;
 import Tablero.Tablero;
+import Vehiculos.Vehiculo;
 
-public class MapaVista { 
+public class MapaVista implements Observer{ 
 	private int esquinasHorizontales;
 	private int esquinasVerticales;
 	private int anchoCalle;
+	private int tamanioManzanaHorizontal;
+	private int tamanioManzanaVertical;
+	private JButton boton;
 	private VentanaVista ventanaDestino;
+	private GestorDeMovimientos gestor;
 	
-	MapaVista(VentanaVista unaVentana, int esquinasHorizontales, int esquinasVerticales){
+	MapaVista(VentanaVista unaVentana, int esquinasHorizontales, int esquinasVerticales, GestorDeMovimientos gestor){
 		this.esquinasHorizontales = esquinasHorizontales;
 		this.esquinasVerticales = esquinasVerticales;
+		this.tamanioManzanaHorizontal = 0;
+		this.tamanioManzanaVertical = 0;
 		this.anchoCalle = 20;
 		this.ventanaDestino = unaVentana;
+		this.gestor = gestor;
+		this.gestor.addObserver(this);
 	}
 
-	public void dibujarMapa(){
-		int tamanioManzanaHorizontal = this.calcularTamanioManzanaHorizontal();
-		int tamanioManzanaVertical = this.calcularTamanioManzanaVertical();
+	public void dibujarMapaConDeterminadaCantidadDePixeles(int cantidadHorizontal,int cantidadVertical){
+		this.tamanioManzanaHorizontal = this.calcularTamanioManzanaHorizontal(cantidadHorizontal);
+		this.tamanioManzanaVertical = this.calcularTamanioManzanaVertical(cantidadVertical);
 
 		for (int i=0; i<=this.esquinasVerticales;i++){
 			for (int j=0; j<=this.esquinasHorizontales;j++){
-				int fila = (int)this.ventanaDestino.getPuntoInicialMapa().getX() + j * (tamanioManzanaHorizontal + this.anchoCalle);
-		        int columna = (int)this.ventanaDestino.getPuntoInicialMapa().getY() + i * (tamanioManzanaVertical + this.anchoCalle);
+				int fila = j * (tamanioManzanaHorizontal + this.anchoCalle);
+		        int columna = i * (tamanioManzanaVertical + this.anchoCalle);
 		        ManzanaVista manzana = new ManzanaVista (tamanioManzanaHorizontal,tamanioManzanaVertical);
 		        manzana.ponerManzana(ventanaDestino,fila,columna);
 		        }
 		}
-		
+		boton = new JButton("Boton de Prueba");
+		boton.setBounds(650,650,40,40);
+		boton.setVisible(true);
+		this.ventanaDestino.add(boton);
 	}
 	
-	private int calcularTamanioManzanaHorizontal(){
-		int pixelesHorizontales = this.ventanaDestino.getPixelesHorizontales() - (int)this.ventanaDestino.getPuntoInicialMapa().getX();
-		return ((pixelesHorizontales)/(this.esquinasHorizontales+1))-(this.anchoCalle);		
+	
+	public void localizarVehiculoEnMapa(JButton boton, Vehiculo unVehiculo){
+		int posicionHorizontalVehiculo = unVehiculo.getPosicion().getColumna();
+		int posicionVerticalVehiculo = unVehiculo.getPosicion().getFila();
+		int pixelHorizontal = (posicionHorizontalVehiculo+1)*this.tamanioManzanaHorizontal+ this.anchoCalle*posicionHorizontalVehiculo ;
+		int pixelVertical = (unVehiculo.getPosicion().getFila()+1)*this.tamanioManzanaVertical + this.anchoCalle*posicionVerticalVehiculo;
+		boton.setBackground(Color.red);
+		boton.setBounds(pixelHorizontal,pixelVertical,20,20);
+		ventanaDestino.add(boton);		
+	}
+	public int getTamanioManzanaHorizontal(){
+		return this.tamanioManzanaHorizontal;
 	}
 	
-	private int calcularTamanioManzanaVertical(){
-		int pixelesVerticales = this.ventanaDestino.getPixelesVerticales() - (int)this.ventanaDestino.getPuntoInicialMapa().getY();
-		return ((pixelesVerticales)/(this.esquinasVerticales+1))-(this.anchoCalle);		
+	public int getTamanioManzanaVertical(){
+		return this.tamanioManzanaVertical;
+	}
+	
+	private int calcularTamanioManzanaHorizontal(int cantidadHorizontal){
+		return ((cantidadHorizontal)/(this.esquinasHorizontales+1))-(this.anchoCalle);		
+	}
+	
+	private int calcularTamanioManzanaVertical(int cantidadVertical){;
+		return ((cantidadVertical)/(this.esquinasVerticales+1))-(this.anchoCalle);		
+	}
+	
+	public void update(Observable t, Object o)
+	{	
+		for (int i=0; i<=this.esquinasVerticales;i++){
+			for (int j=0; j<=this.esquinasHorizontales;j++){
+				int fila = j * (tamanioManzanaHorizontal + this.anchoCalle);
+		        int columna = i * (tamanioManzanaVertical + this.anchoCalle);
+		        ManzanaVista manzana = new ManzanaVista (tamanioManzanaHorizontal,tamanioManzanaVertical);
+		        manzana.ponerManzana(ventanaDestino,fila,columna);
+		        }
+		}
+			
+			
+	
 	}
 		
 
