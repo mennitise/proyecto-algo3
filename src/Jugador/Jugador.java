9356a4c0d1ae353b10315e75d659388f811f7beb
mennitise@gmail.com <mennitise@gmail.com@98f5ffe9-1009-ce00-be44-1651309dea88>
2013-11-26 20:02:28 +0000

    Agregado de serializacion en la clase Jugador

--- src/Jugador/Jugador.java
+++ src/Jugador/Jugador.java
@@ -1,5 +1,8 @@
 package Jugador;
 
+import org.jdom.Attribute;
+import org.jdom.Element;
+
 import Vehiculos.Vehiculo;
 import Excepciones.NumeroNegativoException;
 import Excepciones.StringVacioException;
@@ -72,5 +75,44 @@ public class Jugador {
 		this.vehiculoUtilizado = unVehiculo;
 		this.vehiculoUtilizado.setConductor(this);
 	}
+
+	// Serialización
 	
+	
+	public Element serializarXML() {
+		Element element = new Element("jugador");
+		
+		Attribute attNombre = new Attribute("nombre",this.nombre);
+		Attribute attCantDeMovimientos = new Attribute("cantidadDeMovimientos",((Integer)this.cantidadDeMovimientos).toString());
+		element.getAttributes().add(attNombre);
+		element.getAttributes().add(attCantDeMovimientos);
+		
+		Element elementVehiculo = this.vehiculoUtilizado.serializarXML();
+		element.getChildren().add(elementVehiculo);
+		return element;
+	}
+	
+	
+	public static Jugador cargarDesdeXML(Element element){
+		
+		String nombre           = (String)element.getAttributeValue("nombre");
+		int cantidadMovimientos = Integer.parseInt(element.getAttributeValue("cantidadDeMovimientos"));
+		
+		Element elementVehiculo = (Element)element.getChildren().get(0);
+		Vehiculo unVehiculo     = Vehiculo.cargarDesdeXML(elementVehiculo);
+		
+		Jugador unJugador = null;
+		try {
+			unJugador = new Jugador(nombre, unVehiculo);
+		} catch (StringVacioException e1) {
+			// No ingresará ya que siempre se guardará de una manera válida.
+		}
+		
+		try {
+			unJugador.sumarMovimientos(cantidadMovimientos);
+		} catch (NumeroNegativoException e) {
+			// No ingresará ya que siempre se guardará de una manera válida.
+		}
+		return unJugador;
+	}
 }
