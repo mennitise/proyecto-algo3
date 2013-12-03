8d57d7e16d6009c6492eb0f3cda808cf822a457a
moncholo06@gmail.com <moncholo06@gmail.com@98f5ffe9-1009-ce00-be44-1651309dea88>
2013-12-03 05:30:52 +0000

    Subo refactorizacion de codigo de VentanaPrincipal y un agregado al build.xml. Ahora basta con hacer ant jar jugar y se abre la aplicacion desde la consola

--- src/Vista/VentanaPrincipal.java
+++ src/Vista/VentanaPrincipal.java
@@ -1,5 +1,6 @@
 package Vista;
 
+import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JFrame;
@@ -13,87 +14,91 @@ public class VentanaPrincipal extends JFrame implements Observer{
 	private Juego juego;
 	private MapaJuegoVista mapaJuegoActual;
 	private PanelDeInformacionVista panelDeInformacionDelJugador;
-	private ControladorDeFlechas unListener;
+	private ControladorDeFlechas controladorDeFlechas;
 	
 	public VentanaPrincipal(){
-
-		
-		this.juego = new Juego(); //el controlador se encargara de iniciarlo
-		this.mapaJuegoActual = new MapaJuegoVista() ;
-		this.mapaJuegoActual.setSize(600,600);
-		this.mapaJuegoActual.setVisible(false);
-//		this.mapaJuegoActual.setBounds(0, 0, 600, 600);
-		this.unListener = null;
-		this.add(mapaJuegoActual);
-		ControladorMenuPrincipal control = new ControladorMenuPrincipal(juego, mapaJuegoActual);
-		
-		this.panelInicial = new PanelInicial(control);	
-		this.panelInicial.setBounds(250,150,300,300);	
-		this.panelInicial.setVisible(true);
-		
-		
-			this.juego.addObserver(this);
-			this.juego.addObserver(this.panelInicial);	
-		this.add(panelInicial);
-				
+			
+		this.juego = new Juego(); 
+		this.configurarMapaJuegoActual();
+		this.controladorDeFlechas = null; //Si estuviera creado, estaría leyendo las teclas. Se crea a la hora de jugar
+		this.configurarPanelInicial();
+		this.juego.addObserver(this); //Cada vez que el juego notifique a los obbservadores, esta clase se actualizara segun el metodo update
+		this.juego.addObserver(this.panelInicial); //Idem para el panelIinicial	
 		this.configurarFrame();
-		
-		
 	}
-	
-	private void configurarFrame(){
-		this.setLayout(null);
-		this.setTitle("GPS Challenge");
-		this.setBounds(300,100,800,600);
-		this.setVisible(true);
-		;
-		
-	}
-	
-	
-	
+				
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		
-		if (this.unListener == null){
-			this.unListener = new ControladorDeFlechas (this.juego.getPartida().getGestorDeMovimientos(), this);
-			this.addKeyListener(unListener);
-	    	this.setFocusable(true);
-	    	this.panelDeInformacionDelJugador = new PanelDeInformacionVista(this.juego);
-	    	this.panelDeInformacionDelJugador.setBounds(600, 0, 200, 500);
-	    	this.add(this.panelDeInformacionDelJugador);
-	    
+		//se va a actualizar cuando algun mensaje desde juego llegue aqui 
+		if (this.controladorDeFlechas == null){
+			this.configurarVentanaParaJugar();
 		}
 	
 		if (this.juego.hayPartidaActiva()){	
-				this.panelInicial.setVisible(false);
-				this.juego.getPartida().getGestorDeMovimientos().addObserver(this);
-				this.mapaJuegoActual.setVisible(true);
-				if (this.juego.terminoLaPartida()){
-					this.panelInicial.setVisible(true);
-					this.mapaJuegoActual.setVisible(false);
-					this.removeKeyListener(unListener);
-					this.unListener = null;
-					this.panelDeInformacionDelJugador.setVisible(false);
-				}
-		}else {
-			this.panelInicial.setVisible(true);
-			this.mapaJuegoActual.setVisible(false);
-			this.removeKeyListener(unListener);
-			this.unListener = null;
-			this.panelDeInformacionDelJugador.setVisible(false);
-
-			}
-		}
+			this.ponerVentanaEnEstadoDeJuego();
 	
+		if (this.juego.terminoLaPartida()){
+			this.ponerVentanaEnEstadoInicial();
+				}
+		} else { // aqui resulta que no hay partida activa y que no termino la partida
+			this.ponerVentanaEnEstadoInicial();
+			}
+	}
+	
+	
+	private void configurarVentanaParaJugar(){
+		this.controladorDeFlechas = new ControladorDeFlechas (this.juego.getPartida().getGestorDeMovimientos(), this);
+		this.addKeyListener(controladorDeFlechas);
+    	this.setFocusable(true);
+    	this.panelDeInformacionDelJugador = new PanelDeInformacionVista(this.juego);
+    	this.panelDeInformacionDelJugador.setBounds(600, 0, 200, 570);
+    	this.add(this.panelDeInformacionDelJugador);
+	}
+	
+    private void ponerVentanaEnEstadoDeJuego(){
+		this.panelInicial.setVisible(false);
+		this.juego.getPartida().getGestorDeMovimientos().addObserver(this);
+		this.mapaJuegoActual.setVisible(true);
+
+    }
+	
+	private void configurarFrame(){
+		this.setLayout(null);
+		this.getContentPane().setBackground(Color.blue);
+		this.setTitle("GPS Challenge");
+		this.setBounds(300,100,800,600);
+		this.setVisible(true);			
+	}
+	
+
+	private void configurarMapaJuegoActual(){
+
+		this.mapaJuegoActual = new MapaJuegoVista() ; //Se crea con un estado vacio que luego el controlador ira manejando segun el modelo
+		this.mapaJuegoActual.setSize(600,600); //configuracion para el JFrame
+		this.mapaJuegoActual.setVisible(false); //Configracion para el JFrame//
+		this.add(mapaJuegoActual);
+	}
+	
+	private void configurarPanelInicial(){
+		ControladorMenuPrincipal control = new ControladorMenuPrincipal(this.juego, this.mapaJuegoActual);		
+		this.panelInicial = new PanelInicial(control);	
+		this.panelInicial.setBounds(250,150,300,300);	
+		this.panelInicial.setVisible(true);
+		this.add(this.panelInicial);
+	}	
+	
+	private void ponerVentanaEnEstadoInicial(){		
+		this.panelInicial.setVisible(true);
+		this.mapaJuegoActual.setVisible(false);
+		this.removeKeyListener(this.controladorDeFlechas);
+		this.controladorDeFlechas = null;
+		this.panelDeInformacionDelJugador.setVisible(false);
+	}
 	
 }
-	
-//	public static void main (String[] args){
-//		VentanaPrincipal nuevaVenta = new VentanaPrincipal();
-//	}
-//
 
+
+	
 
 
 	
